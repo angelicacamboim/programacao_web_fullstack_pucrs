9e6f722fa214a57fa08250957799908867af0184
Angelica Bayer <angelicacamboim@gmail.com>
2023-08-02 17:32:15 -0300

    js1

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,10 +4,36 @@ function Soma() {
     document.getElementById("res").innerHTML = "Resposta: " + (e1 + e2);
 }
 
-function testLocalStorage(){
+function testLocalStorage() {
     var nome = document.getElementById("name").value;
-    localStorage.setItem("name",nome);
+    localStorage.setItem("name", nome);
     document.getElementById("local").innerHTML = localStorage.getItem("name");
 }
 
+function buscarCep(event, form) {
+    event.preventDefault();
+    const inputCep = form.cep;
+    if (inputCep) {
+        const cep = inputCep.value;
+        if (cep.length === 8) {
+            const URL = `https://viacep.com.br/ws/${cep}/json/`;
+            fetch(URL)
+                .then(resposta => resposta.json())
+                .then(data => mostrarResposta(data))
+                .catch(erro => console.error(erro));
+        }
+    }
 
+}
+
+function mostrarResposta(cep) {
+    const mensagem = `
+        CEP: ${cep.cep} <br>
+        Logradouro: ${cep.logradouro} <br>
+        Complemento: ${cep.complemento} <br>
+        Bairro: ${cep.bairro} <br>
+        Localidade: ${cep.localidade} <br>
+        Estado: ${cep.uf} 
+    `;
+    document.getElementById("resultCep").innerHTML = mensagem
+}
